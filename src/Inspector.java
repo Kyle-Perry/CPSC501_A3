38189bd86693bf125d9f40b4211ef21143eb2bc8
Kyle Perry <perry.246@gmail.com>
2023-10-28 23:29:38 -0600

    Inspector handles collection printing
    Inspector prints collection classes by element, inspecting any non-string objects along the way.

--- src/Inspector.java
+++ src/Inspector.java
@@ -2,6 +2,7 @@ import java.lang.reflect.Array;
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
 import java.util.ArrayList;
+import java.util.Collection;
 
 public class Inspector {
 	ArrayList<Integer> addressesViewed;
@@ -35,49 +36,78 @@ public class Inspector {
 			if(classObj.isArray()) {
 				for(int i = 0; i < Array.getLength(obj); i++) {
 					cur = Array.get(obj, i);
-					System.out.println(addTabbing() + "Element " + i + " of array: " + cur);
+
+		if(classObj.isArray()) {
+			for(int i = 0; i < Array.getLength(obj); i++) {
+				cur = Array.get(obj, i);
+				System.out.println(addTabbing() + "Element " + i + " of array: " + cur);
 
 
-					if(!classObj.getComponentType().isPrimitive() && !(classObj.getComponentType() == java.lang.String.class) && cur != null) {
-						System.out.println(addTabbing() + "Object reference found in field, inspecting...");
-						System.out.println(addTabbing() + "----------------------------------------------------------------------");
+				if(!classObj.getComponentType().isPrimitive() && !(cur instanceof String) && cur != null) {
+					System.out.println(addTabbing() + "Object reference found in field, inspecting...");
+					System.out.println(addTabbing() + "----------------------------------------------------------------------");
 
-						inspect(cur);
-						System.out.println(addTabbing() + "----------------------------------------------------------------------");
-					}
+					inspect(cur);
+					System.out.println(addTabbing() + "----------------------------------------------------------------------");
 				}
 			}
-			else {
-				fields = classObj.getDeclaredFields();
-				if(fields.length > 0)
-					System.out.println(addTabbing() + "Declared Fields:");
+		}
+		if(obj instanceof Collection) {
+			for(Object o: (Collection<Object>)obj) {
+				int x = 0;
+				System.out.println(addTabbing() + "Element " + x + " of collection: " + o);
 
-				for(Field f: fields) {
-					try {
-						f.setAccessible(true);
-						cur = f.get(obj);
-						System.out.println(addTabbing() + f.toString() + " = " + cur);						
+				if(!o.getClass().isPrimitive() && !(o.getClass() == java.lang.String.class) && o != null) {
 
-						if(!f.getType().isPrimitive() && !(f.getType() == java.lang.String.class) && cur != null) {
+					System.out.println(addTabbing() + "Object reference found in field, inspecting...");
+					System.out.println(addTabbing() + "----------------------------------------------------------------------");
 
-							System.out.println(addTabbing() + "Object reference found in field, inspecting...");
-							System.out.println(addTabbing() + "----------------------------------------------------------------------");
+					inspect(o);
+					System.out.println(addTabbing() + "----------------------------------------------------------------------");
+					x++;
+				}
+			}
+		}
+		else {
+			while(classObj != null) {
 
-							inspect(cur);
-							System.out.println(addTabbing() + "----------------------------------------------------------------------");
+				if(classObj == String.class)
+				{
+					System.out.println(addTabbing() + "String value = " + obj);
+				}
+				else {
+					fields = classObj.getDeclaredFields();
+					if(fields.length > 0)
+						System.out.println(addTabbing() + "Declared Fields:");
 
+					for(Field f: fields) {
+						try {
+							f.setAccessible(true);
+							cur = f.get(obj);
+							System.out.println(addTabbing() + f.toString() + " = " + cur);						
+
+							if(!f.getType().isPrimitive() && !(f.getType() == java.lang.String.class) && cur != null) {
+
+								System.out.println(addTabbing() + "Object reference found in field, inspecting...");
+								System.out.println(addTabbing() + "----------------------------------------------------------------------");
+
+								inspect(cur);
+								System.out.println(addTabbing() + "----------------------------------------------------------------------");
+
+							}
 						}
+						catch(Exception e) {
+							e.printStackTrace();
+						}
+
 					}
-					catch(Exception e) {
-						e.printStackTrace();
-					}
+
 				}
+
+				classObj = classObj.getSuperclass();
+				if(classObj != null)
+					System.out.println("\n" + addTabbing() + "Superclass:");
 			}
-			
-			classObj = classObj.getSuperclass();
-			if(classObj != null)
-				System.out.println("\n" + addTabbing() + "Superclass:");
-			
 		}
 
 		tabbing--;
