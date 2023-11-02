7f570155ddac13731745801e1c7de54eeeac5a2e
Kyle Perry <perry.246@gmail.com>
2023-11-02 17:22:54 -0600

    TestSerializer tests PrimitiveFields
    Test serializer properly tests creation of objects with Primitive fields. Note: for serialization and deserialization, strings are treated as a primitive. (They are stored as a value in the object)

--- src/TestSerializer.java
+++ src/TestSerializer.java
@@ -15,9 +15,60 @@ public class TestSerializer {
 	
 	@Test
 	public void testPrimFields() {
-		TestObj testObj = new TestObj(100, ((short)50), ((long)200), ((byte)64), ((float)3.14), 99999.99, 'a', true, "Hello, world!");		
+		TestObj testObj = new TestObj(100, ((short)50), ((long)200), ((byte)64), ((float)3.14), 99999.99, 'a', true, "Hello, world!");
 		
-		assertTrue(true);
+		Document tDoc = testSer.serialize(testObj);
+		Element root = tDoc.getRootElement();
+		assertEquals(root.getContent().size(), 1);
+		
+		Element objEle = (Element)root.getContent(0);
+		assertEquals(objEle.getAttribute("class").getValue(), "TestObj");
+		
+		Element fieldEle = (Element)objEle.getContent(0);
+		assertEquals(fieldEle.getAttribute("name").getValue(), "val1");
+		assertEquals(fieldEle.getAttribute("declaringclass").getValue(), "TestObj");
+		assertEquals(((Element)fieldEle.getContent(0)).getText(), "100");
+		
+		fieldEle = (Element)objEle.getContent(1);
+		assertEquals(fieldEle.getAttribute("name").getValue(), "val2");
+		assertEquals(fieldEle.getAttribute("declaringclass").getValue(), "TestObj");
+		assertEquals(((Element)fieldEle.getContent(0)).getText(), "50");
+		
+		fieldEle = (Element)objEle.getContent(2);
+		assertEquals(fieldEle.getAttribute("name").getValue(), "val3");
+		assertEquals(fieldEle.getAttribute("declaringclass").getValue(), "TestObj");
+		assertEquals(((Element)fieldEle.getContent(0)).getText(), "200");
+		
+		fieldEle = (Element)objEle.getContent(3);
+		assertEquals(fieldEle.getAttribute("name").getValue(), "val4");
+		assertEquals(fieldEle.getAttribute("declaringclass").getValue(), "TestObj");
+		assertEquals(((Element)fieldEle.getContent(0)).getText(), "64");
+		
+		fieldEle = (Element)objEle.getContent(4);
+		assertEquals(fieldEle.getAttribute("name").getValue(), "val5");
+		assertEquals(fieldEle.getAttribute("declaringclass").getValue(), "TestObj");
+		assertEquals(((Element)fieldEle.getContent(0)).getText(), "3.14");
+		
+		fieldEle = (Element)objEle.getContent(5);
+		assertEquals(fieldEle.getAttribute("name").getValue(), "val6");
+		assertEquals(fieldEle.getAttribute("declaringclass").getValue(), "TestObj");
+		assertEquals(((Element)fieldEle.getContent(0)).getText(), "99999.99");
+		
+		fieldEle = (Element)objEle.getContent(6);
+		assertEquals(fieldEle.getAttribute("name").getValue(), "val7");
+		assertEquals(fieldEle.getAttribute("declaringclass").getValue(), "TestObj");
+		assertEquals(((Element)fieldEle.getContent(0)).getText(), "a");
+		
+		fieldEle = (Element)objEle.getContent(7);
+		assertEquals(fieldEle.getAttribute("name").getValue(), "val8");
+		assertEquals(fieldEle.getAttribute("declaringclass").getValue(), "TestObj");
+		assertEquals(((Element)fieldEle.getContent(0)).getText(), "true");
+		
+		fieldEle = (Element)objEle.getContent(8);
+		assertEquals(fieldEle.getAttribute("name").getValue(), "val9");
+		assertEquals(fieldEle.getAttribute("declaringclass").getValue(), "TestObj");
+		assertEquals(((Element)fieldEle.getContent(0)).getText(), "Hello, world!");
+		
 	}
 	
 	@Test
